6854a5d171b8a5ede816c26f64df6cb8a2b285c4
Jonathan Hudak <hudak.jonathan@gmail.com>
2022-05-25 18:19:15 -0600

    Deno + Remix + Unocss

---
+++ app/root.tsx
@@ -0,0 +1,49 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/deno";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import * as React from "react";
+import reset from "@unocss/reset/tailwind.css";
+
+import unocss from "./uno.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: reset,
+    },
+    {
+      rel: "stylesheet",
+      href: unocss,
+    },
+  ];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+export default function Index() {
+  return (
+    <div className=" bg-gradient-to-r min-h-screen from-violet-500 to-fuchsia-500">
+      <div className="min-h-screen grid place-content-center">
+        <h1 className="color-white text-4xl font-sans">
+          Welcome to Remix + Deno + unocss
+        </h1>
+      </div>
+    </div>
+  );
+}
